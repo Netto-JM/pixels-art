953798c7f6dbc708ebf64bbd4a7fc1f331b75f77
NettoJM <nettojm86@gmail.com>
2022-08-31 17:57:15 -0300

    fourth requisite completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // testing windows size 1366 x 768
+const colorBlocks = document.getElementsByClassName('color');
+const randomizerButton = document.getElementById('button-random-color');
+
+function generateRandomColor() {
+  const colorValues = [];
+  for (let index = 0; index < 3; index += 1) {
+    const colorValue = Math.floor(Math.random() * 256);
+    colorValues[index] = colorValue;
+  }
+  const color = `rgb(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]})`;
+  return color;
+}
+
+function addRandomColors() {
+  for (let index = 1; index < colorBlocks.length; index += 1) {
+    const block = colorBlocks[index];
+    const randomColor = generateRandomColor();
+    block.style.backgroundColor = randomColor;
+  }
+}
+
+randomizerButton.addEventListener('click', addRandomColors);
